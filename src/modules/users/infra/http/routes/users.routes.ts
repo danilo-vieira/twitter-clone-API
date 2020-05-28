a28ba65d234fdacae2a1da17f18dbab812bbbbd1
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-05-28 06:45:04 -0300

    feat(modules): validation for users routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+    },
+  }),
+  usersController.create
+);
 
 export default usersRouter;
