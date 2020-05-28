5637c9a08b091a586d6d5db030c1073a56e93cdb
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-05-28 06:44:36 -0300

    feat(modules): validation for sessions routes

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import SessionController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
 const sessionController = new SessionController();
 
-sessionsRouter.post('/', sessionController.create);
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionController.create
+);
 
 export default sessionsRouter;
