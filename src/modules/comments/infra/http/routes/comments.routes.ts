595217694f922a243e4b0f7a944d6fb1a846f1c2
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-05-28 05:16:51 -0300

    feat(modules): validation for comments routes

--- src/modules/comments/infra/http/routes/comments.routes.ts
+++ src/modules/comments/infra/http/routes/comments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import CommentsController from '../controllers/CommentsController';
@@ -6,13 +7,60 @@ import CommentsController from '../controllers/CommentsController';
 const commentsRouter = Router();
 const commentsController = new CommentsController();
 
-commentsRouter.get('/:postId', commentsController.index);
-commentsRouter.get('/:postId/:commentId', commentsController.show);
+commentsRouter.get(
+  '/:postId',
+  celebrate({
+    [Segments.PARAMS]: {
+      postId: Joi.string().uuid({ version: 'uuidv4' }).required(),
+    },
+  }),
+  commentsController.index
+);
+commentsRouter.get(
+  '/:postId/:commentId',
+  celebrate({
+    [Segments.PARAMS]: {
+      postId: Joi.string().uuid({ version: 'uuidv4' }).required(),
+      commentId: Joi.string().uuid({ version: 'uuidv4' }).required(),
+    },
+  }),
+  commentsController.show
+);
 
 commentsRouter.use(ensureAuthenticated);
 
-commentsRouter.post('/:postId', commentsController.create);
-commentsRouter.put('/:commentId', commentsController.update);
-commentsRouter.delete('/:commentId', commentsController.delete);
+commentsRouter.post(
+  '/:postId',
+  celebrate({
+    [Segments.PARAMS]: {
+      postId: Joi.string().uuid({ version: 'uuidv4' }).required(),
+    },
+    [Segments.BODY]: {
+      content: Joi.string().min(1).max(280).required(),
+    },
+  }),
+  commentsController.create
+);
+commentsRouter.put(
+  '/:commentId',
+  celebrate({
+    [Segments.PARAMS]: {
+      commentId: Joi.string().uuid({ version: 'uuidv4' }),
+    },
+    [Segments.BODY]: {
+      content: Joi.string().min(1).max(280).required(),
+    },
+  }),
+  commentsController.update
+);
+commentsRouter.delete(
+  '/:commentId',
+  celebrate({
+    [Segments.PARAMS]: {
+      commentId: Joi.string().uuid({ version: 'uuidv4' }).required(),
+    },
+  }),
+  commentsController.delete
+);
 
 export default commentsRouter;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
